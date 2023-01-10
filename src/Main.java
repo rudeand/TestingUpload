24e5fac5f083ba45634e8d68da41a2fe0b9015c6
rudeand <a.rudevski@gmail.com>
2023-01-10 10:03:18 -0500

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        System.out.println("Let's try it again-second commit!");
+
+        // write a loop to print numbers from 1 to 100
+        int number=5;
+        while (number<=50){
+            System.out.println(number);
+            number+=5;
+        }
     }
 }
